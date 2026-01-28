6aa18e7a0500f6e9959f8fe4853108e9014d0529
samueljkauff <samueljkauff@gmail.com>
2026-01-28 03:48:14 -0500

    2

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -6,10 +6,12 @@ fn main() {
 mod tests {
     #[test]
     fn slice_out_of_array() {
-        let a = [1, 2, 3, 4, 5];
+        let a: [i32; 5] = [1, 2, 3, 4, 5];
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
         // let nice_slice = ???
+        let nice_slice: [i32; 3] = &a[1..3];
+        
 
         assert_eq!([2, 3, 4], nice_slice);
     }
